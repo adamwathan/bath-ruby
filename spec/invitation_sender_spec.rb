3132b3b2bd9b0c71bd2edc0bd52916c7f2d7fd7f
Ben Orenstein <ben.orenstein@gmail.com>
2015-03-09 18:59:26 -0400

    Extract mailer stubbing

--- spec/invitation_sender_spec.rb
+++ spec/invitation_sender_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe InvitationSender do
   describe '#send' do
     it "sends emails to all invitees in an uploaded CSV" do
+      stub_mailer
       csv = <<-CSV.strip_heredoc
         email,name
         one@example.com,User One
         two@example.com,User Two
       CSV
-      mailer = double('mailer', deliver: true)
-      allow(Mailer).to receive(:invitation).and_return(mailer)
 
       InvitationSender.new(csv, 'hello').deliver
 
@@ -23,6 +22,11 @@ describe InvitationSender do
         'two@example.com',
         'hello'
       )
+    end
+
+    def stub_mailer
+      mailer = double('mailer', deliver: true)
+      allow(Mailer).to receive(:invitation).and_return(mailer)
     end
   end
 end
