443dcf7eb508a1573b897546ff3eb38266d237d8
Ben Orenstein <ben.orenstein@gmail.com>
2015-03-09 19:38:07 -0400

    Call deliver

--- lib/invitations_controller.rb
+++ lib/invitations_controller.rb
@@ -1,7 +1,9 @@
 class InvitationsController < ApplicationController
   def create
-    InvitationSender.new(params[:csv_file].read,
-                         params[:message])
+    InvitationSender.new(
+      params[:csv_file].read,
+      params[:message]
+    ).deliver
 
   end
 end
